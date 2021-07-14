8bfdf16065363f394f7ff8b0d365d76fc860340d
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-15 00:41:30 +0100

    Import and Add image logo to Navbar file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/pasta-logo.png";
 
 function Navbar() {
-	return (
-		<div className='navbar'>
-          <div className='leftSide'>
-			  <img src={}/>
-		  </div>
-		  <div className='rightSide'></div>
-		</div>
-	)
+  return (
+    <div className="navbar">
+      <div className="leftSide">
+        <img src={Logo} />
+      </div>
+      <div className="rightSide"></div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
