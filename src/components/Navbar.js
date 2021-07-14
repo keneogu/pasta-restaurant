0b6eb810928aac3edff3a49c9802d35f620ad32f
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-15 00:34:32 +0100

    Add navbar file to components folder

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+	return (
+		<div className='navbar'>
+          <div className='leftSide'>
+			  <img src={}/>
+		  </div>
+		  <div className='rightSide'></div>
+		</div>
+	)
+}
+
+export default Navbar
