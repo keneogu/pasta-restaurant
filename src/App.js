439f18c63916418e66ac73c430ecd45cdeebb523
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-15 01:40:13 +0100

    Add Router, switch and Route to App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
