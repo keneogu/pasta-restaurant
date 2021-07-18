fb78bdd307b84975fb330f0d5212aa9477048a52
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-18 12:38:02 +0100

    Add Reorder icon for mobile view and hidden links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,29 @@
 import React from "react";
 import Logo from "../assets/pasta-logo.png";
 import { Link } from 'react-router-dom';
+import ReorderIcon from '@material-ui/icons/Reorder';
+import '../styles/Navbar.css';
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide">
         <img src={Logo} />
+        <div className='hiddenLinks'>
+        <Link to='/'>Home</Link>
+        <Link to='/menu'>Menu</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+        </div>
       </div>
       <div className="rightSide">
         <Link to='/'>Home</Link>
         <Link to='/menu'>Menu</Link>
         <Link to='/about'>About</Link>
         <Link to='/contact'>Contact</Link>
+        <button>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
   );
