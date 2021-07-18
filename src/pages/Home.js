1ed87c69111424f853194103087729643e666ca7
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-18 17:02:27 +0100

    Add Home background image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import { Link } from '@material-ui/core'
-import React from 'react'
+import React from 'react';
+import BackImage from '../assets/vermi.png';
 
 function Home() {
 	return (
 		<div className='home'>
-		  <div className='headerContainer'>
+		  <div className='headerContainer' style={{ backgroundImage: `url(${BackImage})` }}>
 			<h1> Xenx Pasta</h1>
 			<p> PASTA TO FIT ANY TASTE</p>
 			<Link to='/menu'>
