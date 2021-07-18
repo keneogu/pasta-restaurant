e0bf2ebe13ea5eda3643373af736be1d82832f84
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-18 11:29:14 +0100

    Add navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../assets/pasta-logo.png";
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
@@ -7,7 +8,12 @@ function Navbar() {
       <div className="leftSide">
         <img src={Logo} />
       </div>
-      <div className="rightSide"></div>
+      <div className="rightSide">
+        <Link to='/'>Home</Link>
+        <Link to='/menu'>Menu</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+      </div>
     </div>
   );
 }
