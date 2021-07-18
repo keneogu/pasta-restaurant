3c522e546b2dbe91c33910e731e874b3c2156242
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-18 16:48:14 +0100

    Add Home h1 and p tag texts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? 'open' : 'close'}>
-        <img src={Logo} />
+        <img src={Logo} alt='logo'/>
         <div className='hiddenLinks'>
         <Link to='/'>Home</Link>
         <Link to='/menu'>Menu</Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
+import { Link } from '@material-ui/core'
 import React from 'react'
 
 function Home() {
 	return (
-		<div>
-			Hello world
+		<div className='home'>
+		  <div className='headerContainer'>
+			<h1> Xenx Pasta</h1>
+			<p> PASTA TO FIT ANY TASTE</p>
+			
+		  </div>
 		</div>
 	)
 }
