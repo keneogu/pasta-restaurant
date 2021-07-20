0c7c89dbc150ed0d7c1c1794aaabdde57ed1386e
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-20 22:30:17 +0100

    Add menulist items

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,34 @@
+import Vermi from "../assets/vermi.png";
+
+export const MenuList = [
+  {
+    name: "Ditalini Pasta",
+    image: Vermi,
+    price: 15.99,
+  },
+  {
+    name: "Ditalini Pasta",
+    image: Vermi,
+    price: 15.99,
+  },
+  {
+    name: "Ditalini Pasta",
+    image: Vermi,
+    price: 15.99,
+  },
+  {
+    name: "Ditalini Pasta",
+    image: Vermi,
+    price: 15.99,
+  },
+  {
+    name: "Ditalini Pasta",
+    image: Vermi,
+    price: 15.99,
+  },
+  {
+    name: "Ditalini Pasta",
+    image: Vermi,
+    price: 15.99,
+  },
+];
