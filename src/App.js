620685645fefed9c8c5888edaade38c3a6d854e4
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-23 11:37:07 +0100

    Add background-image to contact page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/menu" exact component={Menu} />
           <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
         </Switch>
         <Footer />
       </Router>
