cf85b51e6595bbd0ea0368dd0f1d6ba8d69ada0a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-23 11:03:24 +0100

    Add Link from react-router-dom

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Link } from '@material-ui/core'
+import { Link } from 'react-router-dom';
 import React from 'react';
 import BackImage from '../assets/vermi.png';
 import '../styles/Home.css';
