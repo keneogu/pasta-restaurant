dcff7243c7ca718ee4aa84eef1a8b9b235e36c1e
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-23 11:39:31 +0100

    Add contact form

---
+++ src/pages/Contact.js
@@ -0,0 +1,33 @@
+import React from "react";
+import BackImage from "../assets/vermi.png";
+
+function contact() {
+  return (
+    <div>
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${BackImage})` }}
+      ></div>
+      <div className="rightSide">
+        <h1> Contact Us</h1>
+
+        <form id="contact-form" method="POST">
+          <label htmlFor="name">Full Name</label>
+          <input name="name" placeholder="Enter full name..." type="text" />
+          <label htmlFor="email">Email</label>
+          <input name="email" placeholder="Enter email..." type="email" />
+          <label htmlFor="message">Message</label>
+          <textarea
+            rows="6"
+            placeholder="Enter message..."
+            name="message"
+            required
+          ></textarea>
+          <button type="submit"> Send Message</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default contact;
