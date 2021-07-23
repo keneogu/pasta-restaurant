e9b88da968c77c7f0413f05e27c70d6796a5a5cd
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-23 11:04:54 +0100

    Add id to meanuList

--- src/helpers/MenuList.js
+++ src/helpers/MenuList.js
@@ -2,31 +2,37 @@ import Vermi from "../assets/vermi.png";
 
 export const MenuList = [
   {
+    id: 1,
     name: "Ditalini Pasta",
     image: Vermi,
     price: 15.99,
   },
   {
+    id: 2,
     name: "Ditalini Pasta",
     image: Vermi,
     price: 15.99,
   },
   {
+    id: 7,
     name: "Ditalini Pasta",
     image: Vermi,
     price: 15.99,
   },
   {
+    id: 3,
     name: "Ditalini Pasta",
     image: Vermi,
     price: 15.99,
   },
   {
+    id: 5,
     name: "Ditalini Pasta",
     image: Vermi,
     price: 15.99,
   },
   {
+    id: 6,
     name: "Ditalini Pasta",
     image: Vermi,
     price: 15.99,
