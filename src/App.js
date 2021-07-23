6c2a43fba57351b4d42a9c95b5df0f939797e102
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-23 11:08:45 +0100

    Render Menu.js component on App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from './pages/Home';
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/menu" exact component={Menu} />
         </Switch>
         <Footer />
       </Router>
