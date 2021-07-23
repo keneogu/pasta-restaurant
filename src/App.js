cc98c8bada301c9baa5ae87a5e5fa3dcdf8d363a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-07-23 11:10:35 +0100

    Render About page compnent to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
+import About from "./pages/About";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/menu" exact component={Menu} />
+          <Route path="/about" exact component={About} />
         </Switch>
         <Footer />
       </Router>
